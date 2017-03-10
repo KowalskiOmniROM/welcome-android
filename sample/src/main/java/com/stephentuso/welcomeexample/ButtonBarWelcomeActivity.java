d4e63af2015ba6f01dd25ea18bae5d3b9bcec5d1
KowalskiOmniROM <KowalskiOmniROM@users.noreply.github.com>
2017-03-10 20:46:35 +0100

    Update ButtonBarWelcomeActivity.java

--- sample/src/main/java/com/stephentuso/welcomeexample/ButtonBarWelcomeActivity.java
+++ sample/src/main/java/com/stephentuso/welcomeexample/ButtonBarWelcomeActivity.java
@@ -23,8 +23,8 @@ public class ButtonBarWelcomeActivity extends WelcomeActivity {
                 .defaultTitleTypefacePath("Roboto-Bold.ttf")
                 .defaultHeaderTypefacePath("Roboto-Bold.ttf")
                 .bottomLayout(WelcomeConfiguration.BottomLayout.BUTTON_BAR)
-                .page(new TitlePage(R.drawable.ic_image_white, "Title Page"))
-                .page(new BasicPage(R.drawable.ic_image_white, "Basic Page", "A page with a title and description").background(R.color.purple_background))
+                .page(new TitlePage(R.drawable.ic_image_white, (getString(R.string.example_buttonbarsingle_title))))
+                .page(new BasicPage(R.drawable.ic_image_white, (getString(R.string.example_buttonbarsingle_description)), (getString(R.string.example_buttonbarsingle_summary))).background(R.color.purple_background))
                 .build();
     }
 
